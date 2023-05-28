6b1927df542db7260ae6af2f6930d2b208f0acd2
Mehrab Hosseini <mehrab.hosseini14@gmail.com>
2023-05-29 02:23:18 +0330

    Implement useDataBits for manage data

--- src/utils/hooks/useDataBit.ts
+++ src/utils/hooks/useDataBit.ts
@@ -1,6 +1,7 @@
+import { BitDataProps } from '@/types/BitData';
 import { useState } from 'react';
 
-export const useDataBit = (initialData: string) => {
+export const useDataBit = (initialData: string): BitDataProps => {
   const [data, setData] = useState<string>(initialData);
   const handleChangeBit = (newBit: number, index: number) => {
     const _newData = data.split('');
@@ -8,8 +9,10 @@ export const useDataBit = (initialData: string) => {
     const newd = _newData.join('').toString();
     setData(newd);
   };
+  const reset = (data: string) => setData(data);
   return {
     data,
+    reset,
     handleChangeBit,
   };
 };
