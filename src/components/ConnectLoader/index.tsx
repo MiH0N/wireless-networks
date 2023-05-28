fc736bede15d4983d031aade389996ff8eea5191
Mehrab Hosseini <mehrab.hosseini14@gmail.com>
2023-05-29 02:22:32 +0330

    Refactor ConnectLoader component

--- src/components/ConnectLoader/index.tsx
+++ src/components/ConnectLoader/index.tsx
@@ -1,13 +1,16 @@
+import classNames from 'classnames';
 import { FC } from 'react';
 interface IConnectLoaderProps {
   count?: number;
+  className?: string;
 }
 
-export const ConnectLoader: FC<IConnectLoaderProps> = ({ count = 4 }) => {
+export const ConnectLoader: FC<IConnectLoaderProps> = ({ count = 4, className }) => {
   return (
-    <div className='flex  space-x-3 m-auto my-0 '>
+    <div className={classNames('flex  space-x-3 m-auto my-0 ', className)}>
       {[...Array(count)].map((item, index) => (
         <div
+          key={index}
           className='ball w-2 h-2'
           style={{
             animationDelay: `${index * 0.1}s`,
