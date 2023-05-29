03bde39bca0b6f35c4eff495c594b3f6a8d99c30
Mehrab Hosseini <mehrab.hosseini14@gmail.com>
2023-05-29 04:01:27 +0330

    Refactor praity check page

--- src/components/ConnectLoader/index.tsx
+++ src/components/ConnectLoader/index.tsx
@@ -3,15 +3,16 @@ import { FC } from 'react';
 interface IConnectLoaderProps {
   count?: number;
   className?: string;
+  isStatic?: boolean;
 }
 
-export const ConnectLoader: FC<IConnectLoaderProps> = ({ count = 4, className }) => {
+export const ConnectLoader: FC<IConnectLoaderProps> = ({ count = 4, className, isStatic }) => {
   return (
     <div className={classNames('flex  space-x-3 m-auto my-0 ', className)}>
       {[...Array(count)].map((item, index) => (
         <div
           key={index}
-          className='ball w-2 h-2'
+          className={classNames(!isStatic ? 'ball-animation ' : 'ball-static ', 'ball w-2.5 h-2.5')}
           style={{
             animationDelay: `${index * 0.1}s`,
           }}
--- src/pages/ParityCheck.tsx
+++ src/pages/ParityCheck.tsx
@@ -28,28 +28,31 @@ function ParityCheck() {
   return (
     <div className='h-full p-8'>
       <h1>Parity Check</h1>
-      <div className='space-x-8 my-4 flex justify-center items-center mt-8'>
-        <div className='space-y-8'>
-          <ParityData
-            mainData={senderData}
-            parityData={paritySenderData}
-            type='sender'
-            handleChangeData={handleChangeSenderData}
-          />
-          <MyLottie width={200} animationData={radarLottie} className='rotateY-180 min-h-[12rem]' />
-        </div>
-        <ConnectLoader count={10} className='mb-6 self-end' />
-        <div className='space-y-8'>
-          <ParityData
-            mainData={reciverData}
-            parityData={parityReciverData}
-            type='reciver'
-            handleChangeData={handleChangeReciverData}
-            showError={paritySenderData.data !== parityReciverData.data}
-          />
-          <MyLottie width={180} animationData={serverLottie} className='min-h-[12rem] flex items-center' />
-        </div>
+      <div className='space-x-8 my-4 flex justify-between items-center mt-8'>
+        <ParityData
+          mainData={senderData}
+          parityData={paritySenderData}
+          type='sender'
+          handleChangeData={handleChangeSenderData}
+        />
+        <ParityData
+          mainData={reciverData}
+          parityData={parityReciverData}
+          type='reciver'
+          handleChangeData={handleChangeReciverData}
+          showError={paritySenderData.data !== parityReciverData.data}
+        />
       </div>
+      <div className='space-x-8 my-4 flex justify-around items-center mt-8'>
+        <MyLottie width={200} animationData={radarLottie} className='rotateY-180 min-h-[12rem]' />
+        <ConnectLoader count={15} className='mb-6' isStatic/>
+        <MyLottie
+          width={160}
+          animationData={serverLottie}
+          className='min-h-[12rem] flex items-center'
+        />
+      </div>
+
       <Button variant='error' iconComponent={<BiRevision />} onClick={handleReset}>
         Reset
       </Button>
