2db88f2f5ff98aa89012e056a7f0c79ee63b4f15
Mehrab Hosseini <mehrab.hosseini14@gmail.com>
2023-05-31 23:19:31 +0330

    Config routes and create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import ParityCheck from 'pages/ParityCheck';
+import { CRCPage } from './pages/CRCPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Layout } from './pages/Layout';
+import { NoMatch } from './pages/NoMatch';
 
 function App() {
   return (
     <>
-      <ParityCheck />
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Navigate to='/parity-check' replace />} />
+          <Route path='parity-check' element={<ParityCheck />} />
+          <Route path='crc' element={<CRCPage />} />
+          <Route path='*' element={<NoMatch />} />
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ src/pages/CRCPage.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+interface ICRCPageProps {}
+
+export const CRCPage: FC<ICRCPageProps> = props => {
+  return <div></div>;
+};
